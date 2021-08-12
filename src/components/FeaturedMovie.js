5df142627aaa897885f22f4610485a7c10ec8034
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-08-11 23:54:32 -0300

    destaques exibindo informações

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -12,7 +12,17 @@ export default ({item}) => {
       }}>
         <div className="feature--vertical">
           <div className="feature--horizontal">
+            <div className="feature--name">{item.original_name}</div>
+            <div className="feature--info">
+              <div className="feature--pointer">{item.vote_average}pontos</div>
+              <div className="feature--year">2099</div>
+              <div className="feature--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
+            </div>
+            <div className="feature--description">{item.overview}</div>
+            <div className="feature--button">
 
+            </div>
+            <div className="feature--genres"><strong>Gêneros:...</strong></div>
           </div>
         </div>
       </section>
