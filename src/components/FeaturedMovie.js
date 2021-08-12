cf9081a1da3c38d30454711a7d8b5ee5a8502836
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-08-11 23:32:10 -0300

    destaque css basic

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -5,8 +5,16 @@ import './FeaturedMovie.css'
 export default ({item}) => {
   return(
     <div>
-      <section className="feature">
-        <div>{item.original_name}</div>
+      <section className="feature" style={{
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+      }}>
+        <div className="feature--vertical">
+          <div className="feature--horizontal">
+
+          </div>
+        </div>
       </section>
     </div>
   )
