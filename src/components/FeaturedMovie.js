3602f97bdd3f0f24f79e77a6d72297dc531e171f
Felipe Ferreira <filipiferreirasilva789@gmail.com>
2021-08-13 20:23:06 -0300

    Update FeaturedMovie.js

--- src/components/FeaturedMovie.js
+++ src/components/FeaturedMovie.js
@@ -3,6 +3,13 @@ import React from 'react'
 import './FeaturedMovie.css'
 
 export default ({item}) => {
+
+  let firstDate = new Date(item.first_air_date)
+  let genres = []
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name)
+  }
+
   return(
     <div>
       <section className="feature" style={{
@@ -15,14 +22,15 @@ export default ({item}) => {
             <div className="feature--name">{item.original_name}</div>
             <div className="feature--info">
               <div className="feature--pointer">{item.vote_average}pontos</div>
-              <div className="feature--year">2099</div>
+              <div className="feature--year">{firstDate.getFullYear()}</div>
               <div className="feature--seasons">{item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ''}</div>
             </div>
             <div className="feature--description">{item.overview}</div>
             <div className="feature--button">
-
+              <a href={`/watch/${item.id}`}>▶ Assistir</a>
+              <a href={`/watch/${item.id}`}>+ Minha lista</a>
             </div>
-            <div className="feature--genres"><strong>Gêneros:...</strong></div>
+            <div className="feature--genres"><strong>Gêneros:</strong>{genres.join(', ')}</div>
           </div>
         </div>
       </section>
